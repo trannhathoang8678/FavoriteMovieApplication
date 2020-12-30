53463751bbdc827eecbcc1134f69f9a5f75227a2
raven <trannhathoang8678@gmail.com>
2020-12-30 15:36:26 +0700

    create project

---
+++ src/main/java/plusplus/FavoriteMovieAplication/FavoriteMovieAplicationApplication.java
@@ -0,0 +1,13 @@
+package plusplus.FavoriteMovieAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteMovieAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteMovieAplicationApplication.class, args);
+	}
+
+}
