17cb1f2be6ce5dc684f77916b765496f00edd6e1
raven <trannhathoang8678@gmail.com>
2021-01-03 12:13:17 +0700

    update config datasource

--- src/main/java/plusplus/FavoriteMovieAplication/JpaConfig.java
+++ src/main/java/plusplus/FavoriteMovieAplication/JpaConfig.java
@@ -1,8 +1,10 @@
 package plusplus.FavoriteMovieAplication;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -12,14 +14,15 @@ import java.sql.SQLException;
 public class JpaConfig {
     private Connection connection;
     private DataSource ds;
-
+    @Autowired
+    private Environment env;
     @Bean
     public DataSource getDataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-        dataSourceBuilder.url("jdbc:mysql://localhost:3306/springdemodb");
-        dataSourceBuilder.username("root");
-        dataSourceBuilder.password("hoangnt1@");
+        dataSourceBuilder.driverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSourceBuilder.url(env.getProperty("spring.datasource.url"));
+        dataSourceBuilder.username(env.getProperty("spring.datasource.username"));
+        dataSourceBuilder.password(env.getProperty("spring.datasource.password"));
         ds = dataSourceBuilder.build();
         return ds;
     }
--- src/main/java/plusplus/FavoriteMovieAplication/model/MovieInfo.java
+++ src/main/java/plusplus/FavoriteMovieAplication/model/MovieInfo.java
@@ -1,4 +1,17 @@
 package plusplus.FavoriteMovieAplication.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import plusplus.FavoriteMovieAplication.JpaConfig;
+
+@Configuration
 public class MovieInfo {
+    @Autowired
+    JpaConfig jpaConfig;
+    @Bean
+    public void addType()
+    {
+
+    }
 }
